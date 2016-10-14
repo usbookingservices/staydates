1916e96159eb6849398caf1baa361d5054adde42
Iain Baird <iainb@worldbookingservice.co.uk>
2016-10-14 11:18:19 +0100

    Fixed, and made the functions return the right things

--- src/USBooking/StayDates/StayDatesCalculator.php
+++ src/USBooking/StayDates/StayDatesCalculator.php
@@ -3,7 +3,31 @@ namespace USBooking\StayDates;
 
 class StayDatesCalculator
 {
+    /**
+     * Given a check in date in the format 'yyyy-mm-dd' and a stay length, this returns an array of every date of the stay as
+     * an array of strings in the format 'yyyy-mm-dd'.
+     * @param string $checkInDate
+     * @param int $stayLength
+     * @return false|string[]
+     */
     public function getStayDates($checkInDate, $stayLength)
+    {
+        $dates = array();
+        for ($i=0; $i<$stayLength; $i++) {
+            $dates[] = date('Y-m-d', strtotime($checkInDate." +$i days"));
+        }
+
+        return $dates;
+    }
+
+    /**
+     * Given a check in date in the format 'yyyy-mm-dd' and a stay length, this returns the date of check out,
+     * as a string in the format 'yyyy-mm-dd'.
+     * @param string $checkInDate
+     * @param int $stayLength
+     * @return false|string
+     */
+    public function getCheckOutDate($checkInDate, $stayLength)
     {
         return date('Y-m-d', strtotime($checkInDate." +$stayLength days"));
     }
